a8a392295847d6c574ed54cb8aa20ca76bb77add
Math-42 <matheus.v4200@gmail.com>
2022-05-03 15:10:16 -0300

    cli: Create arg parser for each subcommand

---
+++ src/cli/mod.rs
@@ -0,0 +1,47 @@
+use clap::{arg, command, Command};
+
+pub fn cli() -> Command<'static> {
+	return command!()
+		.subcommand_required(true)
+		.arg_required_else_help(true)
+		.allow_external_subcommands(true)
+		.allow_invalid_utf8_for_external_subcommands(true)
+		.subcommand(
+			Command::new("init")
+				.about("Start a new project")
+				.arg(
+					arg!(-n --name <PROJECT_NAME> "Set a custom project name")
+						.required(false)
+						.allow_invalid_utf8(true),
+				)
+				.arg(arg!(-l --local "set the user credentials locally")),
+		)
+		.subcommand(Command::new("build").about("Compile the project"))
+		.subcommand(Command::new("run").about("Runs the project"))
+		.subcommand(
+			Command::new("test").about("Run all test cases").arg(
+				arg!(-n --number <TEST_CASE_NUMBER> "Specify witch test run")
+					.required(false)
+					.allow_invalid_utf8(true),
+			),
+		)
+		.subcommand(Command::new("send").about("Submit the current project to run.codes"))
+		.subcommand(
+			Command::new("credentials")
+				.about("Set the user credentials")
+				.arg(arg!(-l --local "Change only the local credentials").required(false)),
+		)
+		.subcommand(Command::new("secret").about("Get the closed test cases"))
+		.subcommand(Command::new("update").about("Update the data of the exercise"))
+		.subcommand(
+			Command::new("subscribe")
+				.about("Enroll to a new class")
+				.arg(arg!(-c --class_code <CLASS_CODE> "Code of the new class").required(true)),
+		)
+		.subcommand(
+			Command::new("import")
+				.about("import a project from a git repo or from run codes")
+				.arg(arg!(-r --run_codes <RUN_CODES_LINK> "Code of the new class").required(true))
+				.arg(arg!(-r --git <GIT_REPO_LINK> "Code of the new class").required(true)),
+		);
+}
