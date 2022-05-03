a3d0b06867e8ef9232fbf0d74972f82e1fc2c2f3
Math-42 <matheus.v4200@gmail.com>
2022-05-03 15:12:16 -0300

    sub-commands: create all subcommands structure

---
+++ src/sub_commands/subscribe.rs
@@ -0,0 +1,3 @@
+pub fn run(_args: &clap::ArgMatches) {
+	println!("sorry, not implemented yet, but you can contribute in: https://github.com/Math-42/run-cli");
+}
---
+++ src/sub_commands/update.rs
@@ -0,0 +1,4 @@
+
+pub fn run(_args: &clap::ArgMatches) {
+	println!("sorry, not implemented yet, but you can contribute in: https://github.com/Math-42/run-cli");
+}
