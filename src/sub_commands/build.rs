2c8ccda82c1b535546a80c48c22c12ba407b65c9
Math-42 <matheus.v4200@gmail.com>
2022-05-05 17:02:12 -0300

    sub-commands: add the compile and run sub commands

--- src/sub_commands/build.rs
+++ src/sub_commands/build.rs
@@ -1,4 +1,25 @@
+pub fn build_project() -> Result<bool, std::io::Error> {
+	let mut cmd = std::process::Command::new("make")
+		.arg("all")
+		.stdout(std::process::Stdio::inherit())
+		.stderr(std::process::Stdio::inherit())
+		.spawn()?;
+
+	let status = cmd.wait()?;
+
+	if status.success() {
+		return Ok(true);
+	};
+
+	Err(std::io::Error::new(
+		std::io::ErrorKind::Other,
+		"Error building the project",
+	))
+}
 
 pub fn run(_args: &clap::ArgMatches) {
-	println!("sorry, not implemented yet, but you can contribute in: https://github.com/Math-42/run-cli");
+	match build_project() {
+		Ok(_) => println!("Project builded with success!"),
+		Err(_) => println!("Error building the project")
+	}
 }
--- src/sub_commands/run.rs
+++ src/sub_commands/run.rs
@@ -1,3 +1,35 @@
+use crate::sub_commands::build;
+
+pub fn run_project() -> Result<bool, std::io::Error> {
+	let mut cmd = std::process::Command::new("make")
+		.arg("run")
+		.stdout(std::process::Stdio::inherit())
+		.stderr(std::process::Stdio::inherit())
+		.spawn()?;
+
+	let status = cmd.wait()?;
+
+	if status.success() {
+		return Ok(true);
+	};
+
+	Err(std::io::Error::new(
+		std::io::ErrorKind::Other,
+		"Error running the project",
+	))
+}
+
 pub fn run(_args: &clap::ArgMatches) {
-	println!("sorry, not implemented yet, but you can contribute in: https://github.com/Math-42/run-cli");
+	match build::build_project() {
+		Ok(_) => println!("Project builded with success!"),
+		Err(_) => {
+			println!("Error building the project");
+			return;
+		}
+	}
+	
+	match run_project() {
+		Ok(_) => println!("running..."),
+		Err(_) => println!("Error running the project"),
+	}
 }
